f44385cea632e1b0cc42a8c267d0241eb4fe8d37
Dmitry Dzhus <dima@dzhus.org>
2018-07-16 14:59:24 +0100

    feat: diff combinator

--- src/TSA.hs
+++ src/TSA.hs
@@ -1,3 +1,10 @@
 module TSA
 
 where
+
+import Control.Applicative
+
+type Series a = [a]
+
+diff :: Num a => Series a -> Series a
+diff series = getZipList $ (-) <$> ZipList (drop 1 series) <*> ZipList series
