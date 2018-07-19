5d4baa4c4cfa1a15d666ce0645e03ec760963f2e
Dmitry Dzhus <dima@dzhus.org>
2018-07-19 11:37:05 +0100

    feat: Add linearTrend

--- src/TSA.hs
+++ src/TSA.hs
@@ -8,6 +8,7 @@ where
 import Data.Monoid
 import qualified Data.Vector.Unboxed as V
 import qualified Statistics.Sample as S
+import qualified Statistics.Regression as S
 
 type Series e = V.Vector e
 
@@ -25,3 +26,16 @@ extendBy periods f s = s <> forecast periods f s
 
 mean :: Model Double
 mean n s = V.replicate n $ S.mean s
+
+linearTrend :: Model Double
+linearTrend n s =
+  if V.length coeffs == 2
+  then
+    let
+      (slope, bias) = (coeffs V.! 0, coeffs V.! 1)
+    in
+      V.iterateN n (+ slope) (fromIntegral len * slope + bias)
+  else error "linearTrend: unexpected number of regression coefficients"
+  where
+    len = V.length s
+    (coeffs, _) = S.olsRegress [V.generate len fromIntegral] s
