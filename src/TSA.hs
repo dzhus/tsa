ebfa77a715f5a6cb0ef9caa85d96a9271209da31
Dmitry Dzhus <dima@dzhus.org>
2018-07-19 17:17:14 +0100

    feat: Allow predicting existing values

--- src/TSA.hs
+++ src/TSA.hs
@@ -5,7 +5,9 @@ module TSA
 
 where
 
+import Data.Maybe
 import Data.Monoid
+
 import qualified Data.Vector.Unboxed as V
 import qualified Statistics.Sample as S
 import qualified Statistics.Regression as S
@@ -16,16 +18,21 @@ diff :: (Num e, V.Unbox e) => Int -> Series e -> Series e
 diff n series = V.zipWith (-) (V.drop n series) series
 
 -- | Model is a forecasting function.
-type Model e = Int -> Series e -> Series e
+--
+-- If the argument is Nothing, predict existing values.
+type Model e = Maybe Int -> Series e -> Series e
 
+-- | Predict N next points for a time series.
 forecast :: Int -> Model e -> Series e -> Series e
-forecast periods f = f periods
+forecast periods f = f $ Just periods
 
 extendBy :: V.Unbox e => Int -> Model e -> Series e -> Series e
 extendBy periods f s = s <> forecast periods f s
 
 mean :: Model Double
-mean n s = V.replicate n $ S.mean s
+mean n s = V.replicate n' $ S.mean s
+  where
+    n' = fromMaybe (V.length s) n
 
 linearTrend :: Model Double
 linearTrend n s =
@@ -33,8 +40,10 @@ linearTrend n s =
   then
     let
       (slope, bias) = (coeffs V.! 0, coeffs V.! 1)
+      start = fromIntegral (maybe 0 (const len) n) * coeffs V.! 0 + bias
+      n' = fromMaybe len n
     in
-      V.iterateN n (+ slope) (fromIntegral len * slope + bias)
+      V.iterateN n' (+ slope) start
   else error "linearTrend: unexpected number of regression coefficients"
   where
     len = V.length s
